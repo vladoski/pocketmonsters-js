9a5d3269d0a536f81d158cd0cc77604c746ecee3
Vlad Florin Sodringa <vlad@sodringa.com>
2020-06-21 14:15:13 +0200

    added location permission check (doesn't work with API > 28)

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,31 @@
 
 	import "smelte/src/tailwind.css";
 
+	function noPermissionGranted() {
+		alert('This app requires your location to be used');
+		navigator.app.exitApp();
+	}
+
+	// Add location marker on the map and geolocation functionality
+	// Checks if the user gave location permissions, if not then the app can't be used (only on Android)
+	// This needs to be executed on deviceready, so the plugins are available to use
+	document.addEventListener('deviceready', e => {
+		cordova.plugins.permissions.checkPermission(cordova.plugins.permissions.ACCESS_FINE_LOCATION, checkStatus => {
+			if (!checkStatus.hasPermission) {
+				cordova.plugins.permissions.requestPermission(cordova.plugins.permissions.ACCESS_FINE_LOCATION, requestStatus => {
+					console.log('hasPermission: ' + requestStatus.hasPermission);
+					if (!requestStatus.hasPermission) {
+						noPermissionGranted();
+					} else {
+						replace('/');
+					}
+				}, (error) => {
+					noPermissionGranted();
+				});
+			}
+		});
+	});
+
 	// Overwrites and handles Android's system back button behaviour ()
 	document.addEventListener('backbutton', e => {
 		e.preventDefault();
